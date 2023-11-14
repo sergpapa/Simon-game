c029e28e45ba89a02752aec0cefbd7cc27c3054c
sergpapa <>
2023-11-14 14:48:14 +0000

    added event listeners to buttons 1-4

--- assets/scripts/tests/game.test.js
+++ assets/scripts/tests/game.test.js
@@ -53,6 +53,12 @@ describe("newGame works correctly", () => {
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     });
+    test("expect data-listener to be true", () => {
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        }
+    });
 });
 
 describe("gameplay works correctly", () => {
